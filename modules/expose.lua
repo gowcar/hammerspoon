2c2075b86c7f6667e5a97fbd5a4b081fcaff8067
天才 <bluebean.jacky@gmail.com>
2020-05-23 16:41:23 +0800

    完善脚本

--- modules/expose.lua
+++ modules/expose.lua
@@ -2,7 +2,7 @@ hs.expose.ui.highlightThumbnailStrokeWidth = 8
 hs.expose.ui.maxHintLetters = 2
 hs.expose.ui.fitWindowsMaxIterations = 30
 hs.expose.ui.fitWindowsInBackground = true
-expose = hs.expose.new(nil,{showThumbnails=true})
+expose = hs.expose.new(nil,{showThumbnails=false})
 hs.hotkey.bind('ctrl-cmd','e','Expose',function()expose:toggleShow()end)
 hs.hotkey.bind('ctrl-cmd-shift','e','App Expose',function()expose_app:toggleShow()end)
 
