6c6f658cf1fb3ad4fd1208bd3e7477bacfcc6322
天才 <bluebean.jacky@gmail.com>
2020-05-26 08:57:16 +0800

    完善代码

--- config.lua
+++ config.lua
@@ -6,11 +6,11 @@ showMessage = false
 
 -- switcher config
 hs.window.switcher.ui.fontName = 'Monaco'
-hs.window.switcher.ui.textSize = 16
+hs.window.switcher.ui.textSize = 12
 -- hs.window.switcher.ui.onlyActiveApplication = false
--- hs.window.switcher.ui.showTitles = true
+hs.window.switcher.ui.showTitles = false
 -- hs.window.switcher.ui.showThumbnails = true
 hs.window.switcher.ui.thumbnailSize = 96
 -- hs.window.switcher.ui.showSelectedThumbnail = true
--- hs.window.switcher.ui.selectedThumbnailSize = 384
--- hs.window.switcher.ui.showSelectedTitle = true
+hs.window.switcher.ui.selectedThumbnailSize = 384
+hs.window.switcher.ui.showSelectedTitle = true
